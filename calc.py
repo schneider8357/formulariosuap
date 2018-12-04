36f4ce068ffb6a2908432613d8889d93acca426a
Lucas Schneider <schneider8357@hotmail.com>
2018-12-04 08:29:13 -0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3 
+import cgi
+import json
+import requests
+
+urls = { 'token':'https://suap.ifrn.edu.br/api/v2/autenticacao/token/', 'dados':'https://suap.ifrn.edu.br/api/v2/minhas-informacoes/meus-dados/'}
+
+def getToken(autenticacao): # Retorna o token para acesso ao SUAP.
+	response = requests.post(urls['token'], data=autenticacao)
+	if response.status_code == 200:
+		return json.loads(response.content.decode('utf-8'))['token']
+	return None
+
+def getInformacoes(cabecalho): # Retorna os dados utilizando o token.
+	response = requests.get(urls['dados'], headers=cabecalho)
+	if response.status_code == 200:
+		return response.content.decode('utf-8')
+	return None
+
+def autentica(login,senha):
+	autenticacao = { 'username': login, 'password': senha }
+	cabecalho = {'Authorization': 'JWT {0}'.format(getToken(autenticacao))}
+	informacoes = json.loads(getInformacoes(cabecalho))
+	return informacoes
+
+form = cgi.FieldStorage()
+matricula = form.getvalue("valor1")
+senha = form.getvalue("valor2")
+operacao = form.getvalue("operacao")
+
+informacoes = autentica(matricula, senha)
+
+print ("Content-type: text/html\n\n" )
+print ("<html><body>")
+print ("<h1>{}</h1>".format(informacoes['nome_usual']))
+print ("</body></html>")
