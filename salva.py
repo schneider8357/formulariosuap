d072a5167a474ea0584701e374fb774588ce0403
Lucas Schneider <schneider8357@hotmail.com>
2018-12-06 16:04:46 -0300

    Add files via upload

--- salva.py
+++ salva.py
@@ -10,13 +10,21 @@ import psycopg2
 cgitb.enable()
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 
-strConexao = 'dbname=postgres user=postgres host=localhost'
+strConexaoPostgres = "dbname=postgres user=postgres host=localhost password=aluno"
+strConexaoAluno = "dbname=aluno user=postgres host=localhost password=aluno"
+strConexaoServidor = "dbname=servidor user=postgres host=localhost password=aluno"
+strCreateAluno = "create table aluno (alu_matricula bigint not null primary key, " \
+									 "alu_nome varchar(100), "\
+									 "alu_email varchar(200) "\
+									 "alu_campus varchar(50) "\
+									 "alu_curso varchar(200))" 
 
-def dat_exists(nomeTabela):
+
+def db_exists(datname):
 	exists = False
 	try:
-		strSQL = "select datname from pg_database where datname='{}'".format(nomeTabela)
-		con = psycopg2.connect(strConexao+' password=aluno')
+		strSQL = "select datname from pg_database where datname='{}'".format(datname)
+		con = psycopg2.connect(strConexaoPostgres)
 		cur = con.cursor()
 		cur.execute(strSQL)
 		if nomeTabela == cur.fetchone()[0]: exists = True
@@ -25,17 +33,58 @@ def dat_exists(nomeTabela):
 		print (e)
 	return exists
 
-def createTable(tipo):
-	if tipo == aluno: strSQL = ''
+def table_exists(datname, tablename):
+	exists = False
+	try:
+		strSQL = "select datname from pg_table where tableowner='{}' tablename='{}'".format(datname,tablename)
+		con = psycopg2.connect(strConexaoPostgres)
+		cur = con.cursor()
+		cur.execute(strSQL)
+		if nomeTabela == cur.fetchone()[0]: exists = True
+		cur.close()
+	except psycopg2.Error as e:
+		print (e)
+	return exists
 
-form = cgi.FieldStorage()
-matricula = form.getvalue('matricula')
-nome = form.getvalue('nome')
-email = form.getvalue('email')
-campus = form.getvalue('campus')
-vinculo = form.getvalue('vinculo')
+def db_create(datname):
+	try:
+		strSQL = "create database {}".format(datname)
+		con = psycopg2.connect(strConexaoPostgres)
+		cur = con.cursor()
+		cur.execute(strSQL)
+		if nomeTabela == cur.fetchone()[0]: exists = True
+		cur.close()
+	except psycopg2.Error as e:
+		print (e)
+
+def table_create(datname,tipo):
+	if tipo == "Aluno":
+		try:
+			con = psycopg2.connect(strConexaoAluno)
+			cur = con.cursor()
+			cur.execute(strCreateAluno)
+			cur.close
+		except psycopg2.Error as e:
+			print (e)
+	if tipo == "Servidor"
 
 
-print('Content-type: text/html\n\n')
-exists = dat_exists('teste')
-if exists: print('O banco teste existe.')
+def main():
+	form = cgi.FieldStorage()
+	matricula = form.getvalue("matricula")
+	nome = form.getvalue("nome")
+	email = form.getvalue("email")
+	campus = form.getvalue("campus")
+	vinculo = form.getvalue("vinculo")
+
+
+	print("Content-type: text/html\n\n")
+	if db_exists("db_suap"):
+		if table_exists("db_suap",vinculo):
+			print("a tabela aluno existe!")
+		else:
+			table_create("db_suap",vinculo)
+	else:
+		db_create("db_suap")
+
+if __name__ == "__main__": main()
